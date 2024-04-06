99e23a16ff7185f5028b4110e9889de491bd62e7
Goofy9506 <goofy9605@protonmail.com>
2024-04-06 05:43:28 -0500

    add: chalk slabs & lantern recipe

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -14,4 +14,30 @@ world.beforeEvents.worldInitialize.subscribe(({ blockTypeRegistry }) => {
       }
     },
   });
+
+  blockTypeRegistry.registerCustomComponent("natures_spirit:slab", {
+    onPlayerInteract(arg) {
+      const itemStack = arg.player.getComponent("inventory").container.getItem(arg.player.selectedSlot);
+      const block = arg.block;
+      const permutation = block.permutation;
+
+      if (itemStack.typeId === block.typeId && !permutation.getState("natures_spirit:double")) {
+        const verticalHalfState = permutation.getState("minecraft:vertical_half");
+
+        if (verticalHalfState === "top" && arg.face === "Down") {
+          performActions();
+        } else if (verticalHalfState === "bottom" && arg.face === "Up") {
+          performActions();
+        }
+      }
+
+      function performActions() {
+        block.setPermutation(permutation.withState("natures_spirit:double", true));
+        arg.player.runCommand(`gamerule sendcommandfeedback false`);
+        arg.player.runCommand(`clear @s ${itemStack.typeId} 0 1`);
+        arg.player.runCommand(`playsound use.stone @a ~~~ 1 0.8`);
+        arg.player.runCommand(`gamerule sendcommandfeedback true`);
+      }
+    },
+  });
 });
